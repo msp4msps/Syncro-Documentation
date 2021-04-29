50f93beb035987368c0a0943ddb36e38547d9d2d
Nick Ross <49374962+msp4msps@users.noreply.github.com>
2021-04-29 06:08:31 -0600

    Support for >100 customers

--- Microsoft_Syncro_LicenseReport.ps1
+++ Microsoft_Syncro_LicenseReport.ps1
@@ -1,4 +1,4 @@
-﻿Param
+Param
 (
 
 [cmdletbinding()]
@@ -55,7 +55,6 @@ $SyncroAPIKey = $SyncroAPIKey
 
 
 ###FUNCTION TO Get All Customers IN SYNCRO####
-
 function GetAll-Customers () {
 
 <#
@@ -76,15 +75,17 @@ param
 (
     [Parameter(Mandatory=$true)]
     [string]$SyncroSubdomain,
-    [string]$SyncroAPIKey
+    [string]$SyncroAPIKey,
+    [string]$page
 )
 
 
-$url =  "https://$($SyncroSubdomain).syncromsp.com/api/v1/customers?api_key=$($SyncroAPIKey)"
+$url =  "https://$($SyncroSubdomain).syncromsp.com/api/v1/customers?api_key=$($SyncroAPIKey)&page=$($page)"
 $response = Invoke-RestMethod -Uri $url -Method Get -ContentType 'application/json'
 $response
 
 }
+
 
 
 ###Update Documents in Syncro #######
@@ -209,7 +210,12 @@ $response
 ###Fnd All Syncro Customers##########
 Write-Host "Getting All Customers In Syncro"
 
-$SyncroCustomers = (GetAll-Customers -SyncroSubdomain $SyncroSubdomain -SyncroAPIKey $SyncroAPIKey).customers
+$page = 1
+$totalPageCount = (GetAll-Customers -SyncroSubdomain $SyncroSubdomain -SyncroAPIKey $SyncroAPIKey -page 1).meta.total_pages
+$SyncroCustomers  = Do{
+   (GetAll-Customers -SyncroSubdomain $SyncroSubdomain -SyncroAPIKey $SyncroAPIKey).customers
+   $page = $page + 1
+   }Until ($page -gt $totalPageCount)
 Write-Host "Found $($SyncroCustomers.Count) Customers in Syncro" -ForegroundColor Green
 $CustomerObj = forEach ($customer in $SyncroCustomers) {
     Write-Host "Getting domain for $($customer.business_name)"
