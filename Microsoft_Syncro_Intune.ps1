883ec9ced8214d47e2a62a7a6f2f72b858238e06
Nick Ross <49374962+msp4msps@users.noreply.github.com>
2021-05-01 15:38:06 -0600

    Update Microsoft_Syncro_Intune.ps1

--- Microsoft_Syncro_Intune.ps1
+++ Microsoft_Syncro_Intune.ps1
@@ -207,11 +207,12 @@ param
 (
     [Parameter(Mandatory=$true)]
     [string]$SyncroSubdomain,
-    [string]$SyncroAPIKey
+    [string]$SyncroAPIKey,
+    [string]$page
 )
 
 
-$url =  "https://$($SyncroSubdomain).syncromsp.com/api/v1/wiki_pages?api_key=$($SyncroAPIKey)"
+$url =  "https://$($SyncroSubdomain).syncromsp.com/api/v1/wiki_pages?api_key=$($SyncroAPIKey)&page=$($page)"
 $response = Invoke-RestMethod -Uri $url -Method GET -ContentType 'application/json'
 $response
 
